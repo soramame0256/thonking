2fccbb1a94e360ced41c41e53b1afea7fe12bf50
soramame_256 <62879324+soramame0256@users.noreply.github.com>
2021-03-30 06:17:37 +0900

    Update pag.js

--- pag.js
+++ pag.js
@@ -1,17 +1,15 @@
 var http = require('http');
 var html = require('fs').readFileSync('index.html');
 http.createServer(function(req, res) {
-  if(req.method === 'GET') {
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.end(html);
-  } else if(req.method === 'POST') {
-    var data = '';
-    req.on('data', function(chunk) {data += chunk})
-      .on('end', function() {
-        var documentr = document.getElementById('data');
-        documentr.insertAdjacentHTML('beforeend', 'data');
-        console.log(data);
-        res.end(html);
-      });
-    }
-}).listen(3000);
+    if(req.method === 'GET') {
+      res.writeHead(200, {'Content-Type' : 'text/html'});
+      res.end(html);
+    } else if(req.method === 'POST') {
+      var data = '';
+      req.on('data', function(chunk) {data += chunk})
+        .on('end', function() {
+          console.log(data);
+          res.end(html);
+        })
+     }
+  }).listen(3000);
