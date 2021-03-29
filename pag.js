03c9eba253e6b1b959ebacc06cffae182c95837f
soramame_256 <62879324+soramame0256@users.noreply.github.com>
2021-03-30 07:09:41 +0900

    Update pag.js

--- pag.js
+++ pag.js
@@ -1,32 +1,7 @@
-http = require('http'),
-    fs = require('fs'),
-    qs = require('querystring');
-
-http.createServer(function (req, res) {
-    var someFile = "index.html";
-
-    if (req.method === 'POST') {
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-        });
-        req.on('end', function () {
-            var poststr = JSON.stringify(qs.parse(body));
-            fs.readFile(someFile, 'utf8', function (err, data) {
-                if (err) return console.log(err);
-                var processed = data.replace(/var data = {[^}]*};/g, 'var data = ' + poststr + ';');
-                fs.writeFile(someFile, processed, 'utf8', function (err) {
-                    if (err) return console.log(err);
-                    res.write("{status: 'ok'}");
-                    res.end();
-                });
-            });
-        });
-    } else {
-        fs.readFile(someFile, 'utf8', function (err, data) {
-            res.write(data);
-            res.end();
-        });
-    }
-}).listen(4128);
-//program from https://qiita.com/hashrock/items/c03415c005d826537d4a
+function a(){
+    let a = document.getElementById("inpt");
+    let b = document.getElementById("output");
+    b.insertAdjacentHTML("beforeend", a.value);
+}
+var btn = document.getElementById("btn");
+btn.addEventListener("click", a);
