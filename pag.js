bbe3612448299f2d20c284bda58d56e04c5f37bc
soramame_256 <62879324+soramame0256@users.noreply.github.com>
2021-03-30 06:51:56 +0900

    Update pag.js

--- pag.js
+++ pag.js
@@ -1,15 +1,32 @@
-var http = require('http');
-var html = require('fs').readFileSync('index.html');
-http.createServer(function(req, res) {
-  if(req.method === 'GET') {
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.end(html);
-  } else if(req.method === 'POST') {
-    var data = '';
-    req.on('data', function(chunk) {data += chunk})
-       .on('end', function() {
-         console.log(data);
-         res.end(html);
-       })
+http = require('http'),
+    fs = require('fs'),
+    qs = require('querystring');
+
+http.createServer(function (req, res) {
+    var someFile = "index.html";
+
+    if (req.method === 'POST') {
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+        });
+        req.on('end', function () {
+            var poststr = JSON.stringify(qs.parse(body));
+            fs.readFile(someFile, 'utf8', function (err, data) {
+                if (err) return console.log(err);
+                var processed = data.replace(/var data = {[^}]*};/g, 'var data = ' + poststr + ';');
+                fs.writeFile(someFile, processed, 'utf8', function (err) {
+                    if (err) return console.log(err);
+                    res.write("{status: 'ok'}");
+                    res.end();
+                });
+            });
+        });
+    } else {
+        fs.readFile(someFile, 'utf8', function (err, data) {
+            res.write(data);
+            res.end();
+        });
     }
-  }).listen(3000);
+}).listen(4128);
+//program from https://qiita.com/hashrock/items/c03415c005d826537d4a
